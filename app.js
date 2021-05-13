ffc9b61f637df813c27255582c7a6a5b4e9d9321
suraj soan kamble <35141388+surajsoankamble@users.noreply.github.com>
2021-05-13 13:40:55 +0530

    update-DanaceWebsite -tut88
    sending contact data to mongodb
    using mongoose

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const express = require('express'); // Import Express
-const path = require('path'); // Import Path for for working with file and directory paths
-const app = express(); // Making Express App
+// Import Express
+const express = require('express'); 
+
+// Import Path for for working with file and directory paths
+const path = require('path'); 
+
+// Making Express App
+const app = express(); 
+
+// getting-started mongoose import and connect
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/danceContact', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+  });
+
+// mongoose schema to model
+const Contact = mongoose.model('Contact', contactSchema);
+
 const port = 8000; // Port Number to run server on
 
 
@@ -24,6 +46,21 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/contact',(req,res)=>{
+    res.status(200).render('contact.pug');
+});
+
+// post - form data to database
+app.post('/contact',(req,res)=>{
+    // getting datta
+    var myData= new Contact(req.body);
+    // saving data
+    myData.save().then(()=>{
+        console.log('data sent')
+    }).catch(()=>{
+        res.status(400).send('data not sent')
+    })
+
+
     res.status(200).render('contact.pug');
 });
 
